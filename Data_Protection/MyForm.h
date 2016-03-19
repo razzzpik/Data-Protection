32775828834e1e8159b8393a74a9eaaab1eebe1e
razpik <razpik@live.ru>
2016-03-19 17:05:47 +0300

    Функции AES

--- Data_Protection/MyForm.h
+++ Data_Protection/MyForm.h
@@ -8,6 +8,8 @@ namespace Data_Protection {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::IO;
+
 
 	/// <summary>
 	/// Сводка для MyForm
@@ -34,6 +36,19 @@ namespace Data_Protection {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Label^  label4;
+
+	protected:
 
 	protected:
 
@@ -50,18 +65,176 @@ namespace Data_Protection {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(250, 98);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Обзор...";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(23, 100);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->ReadOnly = true;
+			this->textBox1->Size = System::Drawing::Size(198, 20);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(23, 25);
+			this->textBox2->MaxLength = 3;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 2;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(23, 81);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(74, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Путь к файлу";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(23, 6);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(167, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Длина ключа (128, 192 или 256)";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(231, 25);
+			this->textBox3->MaxLength = 10;
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 5;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(231, 6);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(90, 13);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"Ключевое слово";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(66, 144);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(91, 23);
+			this->button2->TabIndex = 7;
+			this->button2->Text = L"Шифровать";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(196, 144);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(91, 23);
+			this->button3->TabIndex = 9;
+			this->button3->Text = L"Расшифровать";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(153, 293);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(35, 13);
+			this->label4->TabIndex = 10;
+			this->label4->Text = L"label4";
+			this->label4->Visible = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(359, 318);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	Stream^ myStream;
+	int StreamLength;
+      OpenFileDialog^ openFileDialog1 = gcnew OpenFileDialog;
+
+      openFileDialog1->InitialDirectory = "c:\\";
+      openFileDialog1->Filter = "txt files (*.txt)|*.txt";
+      openFileDialog1->FilterIndex = 1;
+      openFileDialog1->RestoreDirectory = true;
+
+      if ( openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK )
+      {
+         if ( (myStream = openFileDialog1->OpenFile()) != nullptr )
+         {
+			 // Insert code to read the stream here.
+			StreamLength = myStream->Length;
+			// считывание из потока
+			textBox1->Text = openFileDialog1->FileName;
+            myStream->Close();
+         }
+      }
+}
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+	
+	/*
+		запись ключа переменную
+		создание in и out массива unsigned char
+	*/
+	/*String ^MainKey = textBox1->Text;
+	for (int i = 0; i < MainKey->Length; i++)
+		Key[i] = System::Convert::ToChar(MainKey[i]);
+	MainNr = System::Int32::Parse(textBox2->Text);
+	Nr = MainNr;
+	KeyExpansion();
+	Cipher();
+	label4->Text = "Шифрование выполнено успешно. Данные записаны в файл *.txt";
+	label4->Visible = true;*/
+}
+
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+	/*Nr = MainNr;
+	KeyExpansion();
+	InvCipher();
+	label4->Text = "Расшифрование выполнено успешно. Данные записаны в файл *.txt";*/
+}
+};
 }
